a47c1c68f7332de65687a0a63c5dc17ad2dd84a5
Julio Cruz <atlascruz009@gmail.com>
2019-02-12 13:32:00 -0800

    modified work css, images and html

---
+++ assets/work.js
@@ -0,0 +1,27 @@
+const work = [{
+        'header': 'Acacia Dance Floor',
+        'text': 'lorem ipsum testing',
+        'image': '../img/acacia.png',
+        'link': 'https://acaciadancefloor.com/'
+    },
+    {
+        'header': 'Silver Lining Computing',
+        'text': 'lorem ipsum testing',
+        'image': '../img/silverlining.png',
+        'link': 'https://www.silverliningcomputing.com/'
+    },
+    {
+        'header': 'Bianca Hernandez & Hernandez Britto Wedding',
+        'text': 'lorem ipsum testing',
+        'image': '../img/wedding.png',
+        'link': 'https://nandoandbianca.com/'
+    },
+    {
+        'header': 'Dr. Luis G. Vargas DDS',
+        'text': 'lorem ipsum testing',
+        'image': '../img/vargas.png',
+        'link': 'http://www.vargasdds.com/'
+    }
+]
+
+module.exports = work;
