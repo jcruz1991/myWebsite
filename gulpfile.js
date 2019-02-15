896786c00058476b1039ed673ca6cc578da92126
Julio Cruz <atlascruz009@gmail.com>
2019-02-15 15:02:30 -0800

    css modifications and added aos

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglifycss = require('gulp-uglifycss');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const gutil = require('gulp-util');
+const uglify = require('gulp-uglify');
+
+gulp.task('js', () => {
+    gulp
+        .src('./public/js/*.js')
+        .pipe(
+            babel({
+                presets: ['es2015']
+            })
+        )
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./public/js/'))
+        .on('error', function (err) {
+            gutil.log(gutil.colors.red('[Error]'), err.toString());
+        });
+});
 
 gulp.task('sass', function () {
     return gulp.src('./public/scss/**/*.scss')
@@ -19,6 +41,10 @@ gulp.task('css', function () {
 
 gulp.task('watch:sass', function () {
     return gulp.watch('./public/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch:js', () => {
+    gulp.watch('./public/js/**/*.js', ['js']);
 });
 
 gulp.task('run', ['sass', 'css']);
